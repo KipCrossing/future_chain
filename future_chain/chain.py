5be8410e495bfe98ae2c25fdf7b7e57dcab6c801
kipling <kip.crossing@gmail.com>
2021-01-21 22:50:33 +1100

    Some read and write functions

--- future_chain/chain.py
+++ future_chain/chain.py
@@ -1,1 +1,19 @@
-# Heat death
+import json
+
+def read_chain(chain_uri):
+    with open(chain_uri) as chain_file:
+        chain = json.load(chain_file)
+        print(chain)
+
+read_chain("chain.json")
+
+def chain_write(chain_uri, info_dict):
+    chain = []
+    with open(chain_uri) as chain_file:
+        chain: list = json.load(chain_file)
+        chain.append(info_dict)
+    chain_file.close()
+    with open(chain_uri, "w") as chain_file:
+        json.dump(chain, chain_file)
+
+chain_write("chain.json", {"info":10})
